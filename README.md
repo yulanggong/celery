d1f4740e22e1245bdcdd3104a833a2d47c223c20
yulanggong <yulanggong@gmail.com>
2017-04-18 18:28:20 +0800

    Add some styles

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # 🌿于老爷子的芹菜
 一个解除网页禁用复制的小书签。
 
+> 一般的锁抬手拿根面条就能捅开，银行保险柜一根芹菜就捅开了，拿包方便面能出入任何一个小区。—— 郭德纲相声
+
 [yulanggong.github.io/celery](https://yulanggong.github.io/celery)
--- build.js
+++ build.js
@@ -1,12 +1,11 @@
 var UglifyJS = require('uglify-js');
 var fs = require('fs');
 
-var html = fs.readFileSync('./index.html').toString();
-
 var source = UglifyJS.minify('./celery.js', {
   output: { quote_style: 1 }
 }).code;
+
+var html = fs.readFileSync('./index.html').toString();
 html = html.replace(/javascript:([^"])+"/, 'javascript:'+ source +'"');
-debugger
 
 fs.writeFileSync('./index.html', html);
